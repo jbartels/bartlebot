a87a02d2b436c6d60fba36402b99951fb9e989ce
Patrick <patrick@eefy.net>
2011-11-15 19:15:46 -0600

    Added bartlebot.py, and updated to include license header in a few places

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #
-#   Copyright [2011] [Patrick Ancillotti]
+#   Copyright [2011] [Jason Bartels]
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
